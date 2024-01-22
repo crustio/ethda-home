08cb0e9a12aec59e7763fb1f8c94685edb332a26
lizhiyong <1679963578@qq.com>
2024-01-22 11:31:28 +0800

    fix

--- app/blob-TX/page.tsx
+++ app/blob-TX/page.tsx
@@ -46,7 +46,7 @@ const ContentBox = styled(Wrapper)(({}) => ({
 const BlobTX = () => {
   const [clickStart, setIsClickStart] = useState(false)
   const [address, setAddress] = useState<string | undefined>('')
-  const [loading, setLoading] = useState<any>({ loading: false, success: false, error: true })
+  const [loading, setLoading] = useState<any>({ loading: false, success: false, error: false })
   const inputImgRef = useRef<HTMLInputElement>(null)
   const [file, setFile] = useState<File | undefined | null>(null)
   const [selectedBlob, setSelectedBlob] = useState<boolean>(true)
