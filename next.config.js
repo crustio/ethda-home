10bf9145ec95f11cfc676f0876aca2b81e11af19
eericxu <2681350846@qq.com>
2024-07-25 13:55:28 +0800

    fix read file on mobile

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: false,
+  
   output: 'export',
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
